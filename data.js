2ca6f946d91474a11868f47b3e402fd6a9b42a5a
Hamza Bendahmane <hb.tech.business@gmail.com>
2021-09-13 19:31:18 +0200

    ddddf

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 module.exports = {
   token: '1999353773:AAEH8cYpIpx87fn5-FJ1MxOBr2Hq6Ceh7k0', // you can get it in @BotFather
   mongoLink: 'mongodb+srv://botuser:botBOT99@cluster0.ukkqd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', // e.g. Mongo Atlas
-  channel: '@invitchan', 
+  channel: '-1500253527', 
   chanLink: 't.me/invitchan', // can get this in channel`s settings
   paymentsChan: 'paayc', // test chan
   inviteLink: 't.me/otmanPubgBot?start=params', // params - only user id or something extra
