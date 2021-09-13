49f233d7ef8c2ee753610fdb7c8769449bf91ea2
Hamza Bendahmane <hb.tech.business@gmail.com>
2021-09-13 11:35:13 +0200

    ddd

---
+++ data.js
@@ -0,0 +1,10 @@
+module.exports = {
+  token: '1999353773:AAEH8cYpIpx87fn5-FJ1MxOBr2Hq6Ceh7k0', // you can get it in @BotFather
+  mongoLink: 'mongodb+srv://botuser:botBOT99@cluster0.ukkqd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', // e.g. Mongo Atlas
+  channel: '-1500253527', 
+  chanLink: 't.me/invitchan', // can get this in channel`s settings
+  paymentsChan: 'paayc', // test chan
+  inviteLink: 't.me/otmanPubgBot?start=params', // params - only user id or something extra
+  admins: ['hamza_hb'], //TODO: add the id here
+  kh: 'hamza_hb'
+}
