08adf7c8bc546a00fa57af90b209bbe7921d4872
Hamza Bendahmane <hb.tech.business@gmail.com>
2021-09-13 12:11:49 +0200

    ddd

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
-  db = client.db('test')
+  db = client.db('refbot')
   bot.startWebhook('/refbot', null, 2104)
    bot.startPolling()
   //client.close();
