78972bcbe54abdbefc107c824810ca223f956ebc
Hamza Bendahmane <hb.tech.business@gmail.com>
2021-09-13 16:56:19 +0200

    ddddf

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   text: {
-    hello: 'It`s greeting text. Here is user`s referal link: t.me/otmanPubgBot?start=', // then index.js adds here user`s id
-    invite: 'It`s text, which shows when user share link. Here is also user`s referal link: t.me/otmanPubgBot?start='
+    hello: 'It`s greeting text. Here is user`s referal link: t.me/invitchan?start=', // then index.js adds here user`s id
+    invite: 'It`s text, which shows when user share link. Here is also user`s referal link: t.me/invitchan?start='
   }
 
   // any other configs you need
